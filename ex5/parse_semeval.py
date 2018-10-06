f9959e6b64ab83db11302634fbaaa04d2e1f57d0
ecly <ecly@mailbox.org>
2018-10-06 20:59:44 +0200

    simplify parsing and add parsed test/train data

--- ex5/parse_semeval.py
+++ ex5/parse_semeval.py
@@ -7,33 +7,27 @@ TRAIN_FILE = "data/training/TRAIN_FILE.TXT"
 PATTERN = r'(\d+)\s"(.*<e1>(.*)<\/e1>.*<e2>(.*)<\/e2>.*)"'
 TEST_SPLIT = 0.1
 
+def parse_tokens(tokens):
+    # retrieve indices of relations subtracting the
+    # token count we remove from filtered prior to their position
+    idx1 = tokens.index("e1") - 1
+    idx2 = tokens.index("e2") - 7
+    filtered = [x for x in tokens if not re.match(r'<|(/?e(1|2))|>', x)]
+
+    return " ".join(filtered), idx1, idx2
+
+
 def parse_instance(instance):
-    raw, rel = instance.split('\n')[:2]
-    rel_class = re.sub(r'\(.*\)', "", rel)
+    raw, relation = instance.split('\n')[:2]
+    relation_cls = re.sub(r'\(.*\)', "", relation)
     match = re.match(PATTERN, raw)
+    sentence_idx = match.group(1).strip()
+    sentence = match.group(2).strip()
+    filtered, idx1, idx2 = parse_tokens(word_tokenize(sentence))
+    if "(e2,e1)" in relation:
+        idx1, idx2 = idx2, idx1
 
-    sent_id = match.group(1).strip()
-    sent = match.group(2).strip()
-    rel1 = match.group(3).strip()
-    rel2 = match.group(4).strip()
-
-    new_rel1 = rel1.replace(" ", "_")
-    new_rel2 = rel2.replace(" ", "_")
-
-    if re.match(".*(e2,e1)", rel):
-        new_rel1, new_rel2 = new_rel2, new_rel1
-
-    sent = sent.replace(rel1, new_rel1)
-    sent = sent.replace(rel2, new_rel2)
-    trimmed = re.sub(r'<(\/)?e\d>', '', sent)
-    tokens = word_tokenize(trimmed)
-    if new_rel1 not in tokens or new_rel2 not in tokens:
-        #this handles doves<e2>moles</e2> corner case for now
-        return []
-
-    idx1 = str(tokens.index(new_rel1))
-    idx2 = str(tokens.index(new_rel2))
-    return [sent_id, rel_class, idx1, idx2, trimmed]
+    return [sentence_idx, relation_cls, str(idx1), str(idx2), filtered]
 
 
 def print_instances_to_file(inst, filename):
@@ -41,6 +35,7 @@ def print_instances_to_file(inst, filename):
     with open(filename, 'a') as out_file:
         for i in inst:
             out_file.write('\t'.join(i) + '\n')
+
 
 def output_instances(inst):
     """ Splits instances into train/test and prints to files """
@@ -51,8 +46,12 @@ def output_instances(inst):
     print_instances_to_file(test, "test.txt")
     print_instances_to_file(train, "train.txt")
 
-if __name__ == '__main__':
+
+def main():
     nltk.download('punkt')
     with open(TRAIN_FILE) as f:
-        INSTANCES = [parse_instance(i.strip()) for i in f.read().strip().split('\n\n')]
-        output_instances(INSTANCES)
+        instances = [parse_instance(i.strip()) for i in f.read().strip().split('\n\n')]
+        output_instances(instances)
+
+if __name__ == '__main__':
+    main()
